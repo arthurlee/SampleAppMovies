717d2680cfc196c79e75ebefad123dc0564e8775
arthurlee <boyal2008@gmail.com>
2016-07-08 21:40:18 +0800

    show picture ok and load real data

--- index.ios.js
+++ index.ios.js
@@ -7,13 +7,23 @@
 // http://i.imgur.com/UePbdph.jpg
 // https://facebook.github.io/react/img/logo_og.png
 
- var MOCKED_MOVIES_DATA = [{
-	 title: 'Title',
-	 year: '2015',
-	 posters: {
-		 thumbnail: 'https://facebook.github.io/react/img/logo_og.png'
-	 }
- }];
+//  The resource could not be loaded because the App Transport Security policy requires the use of a secure connection.
+
+
+ // var MOCKED_MOVIES_DATA = [{
+ //  title: 'Title',
+ //  year: '2015',
+ //  posters: {
+ // 	 thumbnail: 'https://facebook.github.io/react/img/logo_og.png'
+ //  }
+ // }];
+
+ /**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 
 import React, { Component } from 'react';
 import {
@@ -25,12 +35,56 @@ import {
 } from 'react-native';
 
 class SampleAppMovies extends Component {
-	render() {
-		var movie = MOCKED_MOVIES_DATA[0];
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: null
+		};
+	}
 
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData() {
+		fetch(REQUEST_URL)
+			.then((response) => response.json())
+			.then((responseData) => {
+				this.setState({
+					movies: responseData.movies
+				})
+			})
+			.done();
+	}
+
+	render() {
+		if ( ! this.state.movies) {
+			return this.renderLoadingView();
+		}
+
+		var movie = this.state.movies[0];
+		console.log(movie.posters.thumbnail);
+		return this.renderMovie(movie);
+	}
+
+	renderLoadingView() {
 		return (
 			<View style={styles.container}>
-				<Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}} />
+				<Text>
+					Loading movies...
+				</Text>
+			</View>
+		)
+	}
+
+	renderMovie(movie) {
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.thumbnail}
+					source={{uri: movie.posters.thumbnail}}
+					onError={(e) => console.log(e.nativeEvent.error) }
+				/>
 				<View style={styles.rightContainer}>
 					<Text style={styles.title}>{movie.title}</Text>
 					<Text style={styles.year}>{movie.year}</Text>
@@ -38,6 +92,7 @@ class SampleAppMovies extends Component {
 			</View>
 		);
 	}
+
 }
 
 const styles = StyleSheet.create({
